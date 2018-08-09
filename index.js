24a50fd991f25457686868a6326b027486100c08
Volker Mische <volker.mische@gmail.com>
2018-08-09 16:39:51 +0200

    Refactor a bit

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const fs = require('fs').promises
 // The number of spaces used to indent the children
 const INDENTATION = 2
 
-const processLine = (line, prevLevel, tree) => {
+const processLine = (line, tree) => {
+  const prevDepth = tree.length === 0 ? -1 : tree[tree.length - 1].depth
   // Split between indentation and the rest
   const splitIndentation = line.match(/^( *)(.+)/)
-  const level = splitIndentation[1].length / INDENTATION
-  const data = splitIndentation[2]
+  const depth = splitIndentation[1].length / INDENTATION
+  // TODO vmx 2018-08-09: do more robust parsing
+  const meta = splitIndentation[2].split(' ', 1)[0]
+  const data = splitIndentation[2].substr(meta.length)
 
-
-  if (level === prevLevel) {
+  if (depth === prevDepth) {
     const toAdd = tree.pop()
     console.log('adding to IPLD1:', toAdd)
 
@@ -22,11 +24,11 @@ const processLine = (line, prevLevel, tree) => {
     // Store resulting hash in object where the name is the user defined name and the value is the hash
   }
   // Going deeper
-  else if (level > prevLevel) {
+  else if (depth > prevDepth) {
     // Push the current line after we processed the previous one
   }
   // Going up again, hence write what we had
-  // if (level < prevLevel)
+  // if (depth < prevDepth)
   else {
     const toAdd = tree.pop()
     console.log('adding to IPLD2:', toAdd)
@@ -40,16 +42,18 @@ const processLine = (line, prevLevel, tree) => {
     // Store resulting hash in object where the name is the user defined name and the value is the hash
   }
 
-  tree.push(data)
+  tree.push({
+    depth,
+    data,
+    meta
+  })
 
 
-  return level
+  return depth
 }
 
 const main = async (argv) => {
   const filename = argv[2]
-  // The current indentation level => depth of the node within the tree
-  let depth = -1
   // The tree is represented as a list of object of the shape
   // {
   //   depth: the depth of the tree,
@@ -66,7 +70,7 @@ const main = async (argv) => {
       continue
     }
 
-    depth = processLine(line, depth, tree)
+    processLine(line, tree)
     //console.log(level, line, tree)
   }
 
